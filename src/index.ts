66a9d5f10cec67e2f69316dd90062d7a3eb70ca6
krishkh <krishkhanna00@gmail.com>
2025-03-05 16:18:41 +0530

    setting up prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const app = express();
@@ -14,11 +15,41 @@ async function init() {
         hello: String
         say(name: String): String
       }
-    `,
+      type Mutation {
+        createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+      }
+    `, // Schema
     resolvers: {
       Query: {
         hello: () => "Hello, world!",
         say: (_, { name }) => `Hello, ${name}!`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              firstName,
+              lastName,
+              email,
+              password,
+              salt: "salt",
+            },
+          });
+          return true;
+        },
       },
     },
   });
